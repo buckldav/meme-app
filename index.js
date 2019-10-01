45d393c81b8b0d53768ce9fe23fce6c685f9f9a8
buckldav <runalldayandnight@gmail.com>
2019-10-01 17:07:30 -0600

    Camera

--- index.js
+++
@@ -1,1 +0,0 @@
-import webcam from 'webcamjs'
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Force https connection
-if (window.location.protocol == "http:") {
-  console.log("You are not connected with a secure connection.")
-  console.log("Reloading the page to a Secure Connection...")
-  window.location = document.URL.replace("http://", "https://");
-}
+// if (window.location.protocol == "http:") {
+//   console.log("You are not connected with a secure connection.")
+//   console.log("Reloading the page to a Secure Connection...")
+//   window.location = document.URL.replace("http://", "https://");
+// }
 
-if (window.location.protocol == "https:") {
-  console.log("You are connected with a secure connection.")
-}
+// if (window.location.protocol == "https:") {
+//   console.log("You are connected with a secure connection.")
+// }
 
 
 // Register service worker
